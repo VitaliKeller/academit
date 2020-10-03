47db5e3a966f04574ce5f9175eb07afc38deb4d3
v.keller <v.keller@inbox.ru>
2020-10-03 17:02:39 +0700

    Дополнил main. Создал Range.java

--- Vector/src/Main.java
+++ Vector/src/Main.java
@@ -2,13 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello!");
 
+        // первый отрезок
         System.out.println("Введите начало (включительно) первого отрезка: ");
-        double firstRangeFrom = scanner.nextDouble();
-
+        double from = scanner.nextDouble();
         System.out.println("Введите конец (включительно) первого отрезка: ");
-        double firstRangeTo = scanner.nextDouble();
+        double to = scanner.nextDouble();
+
+        Range range1 = new Range(from,to);
+
+
+        // число для проверки вхождения
+        System.out.println("Введите число, для проверки его вхождения в отрезок: ");
+        double numberToChek = scanner.nextDouble();
+
+        // второй отрезок
+        System.out.println("Введите начало (включительно) второго отрезка: ");
+        from = scanner.nextDouble();
+        System.out.println("Введите конец (включительно) второго отрезка: ");
+        to = scanner.nextDouble();
+
+        Range range2 = new Range(from,to);
+
     }
 }
