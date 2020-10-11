c48991cbbeed968fef538af9238a689df62e15d7
v.keller <v.keller@inbox.ru>
2020-10-11 16:20:19 +0700

    Поправил toString на фигурные скобки {}, добавил имя вектора в класс

--- Vector/src/ru/kellervitali/main/Main.java
+++ Vector/src/ru/kellervitali/main/Main.java
@@ -4,22 +4,22 @@ import ru.kellervitali.vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Vector vector1 = new Vector(new double[] {1, 4, 2});
-        Vector vector2 = new Vector(new double[] {4, 2, 1, 5});
-        Vector vector3 = new Vector(vector2);
-        Vector vector4 = new Vector(new double[]{3, 5, 7}, 5);
+        Vector vector1 = new Vector("vector1", new double[]{1, 4, 2});
+        Vector vector2 = new Vector("vector2", new double[]{4, 2, 1, 5});
+        Vector vector3 = new Vector("vector3", vector2);
+        Vector vector4 = new Vector("vector4", new double[]{3, 5, 7}, 5);
 
-        System.out.println("vector1 = " + vector1);
-        System.out.println("vector2 = " + vector2);
-        System.out.println("vector3 = " + vector3);
-        System.out.println("vector4 = " + vector4);
+        System.out.println(vector1);
+        System.out.println(vector2);
+        System.out.println(vector3);
+        System.out.println(vector4);
         //System.out.println("vector5 = " + vector5);
 
         try {
-            Vector vector5 = new Vector(2);
+            Vector vector5 = new Vector("vector5", 2);
             vector5.setCoordinates(new double[]{1, 2});
-            System.out.println("vector5 = " + vector5);
-            Vector vector666 = new Vector(-1);
+            System.out.println(vector5);
+            Vector vector666 = new Vector("vector666", -1);
         } catch (Throwable e) {
             System.out.println(e.getMessage());
         }
--- Vector/src/ru/kellervitali/vector/Vector.java
+++ Vector/src/ru/kellervitali/vector/Vector.java
@@ -3,32 +3,40 @@ package ru.kellervitali.vector;
 import java.util.Arrays;
 
 public class Vector {
-    // Поля объекта
+    // ================= Поля объекта
+    private final String vectorName;
     private double[] coordinates;
 
-    // Конструкторы
-    public Vector(int dimension) {      // 1.a
+    // ================= Конструкторы
+    // 1.a. Vector(n) – размерность n, все компоненты равны
+    public Vector(String vectorName, int dimension) {
         if (dimension <= 0) {
             throw new IllegalArgumentException("Ошибка! dimension должно быть \">0\"!");
         }
 
+        this.vectorName = vectorName;
         this.coordinates = new double[dimension];
     }
 
-    public Vector(Vector vector) {    // 1.b
+    // 1.b. Vector(Vector) – конструктор копирования
+    public Vector(String vectorName, Vector vector) {
         //this.coordinates = new double[]{vector.getCoordinates()}; // todo так не работает... Разобрать как правильно предать, неужели каждую отдельно?
+        this.vectorName = vectorName;
         this.coordinates = Arrays.copyOf(vector.getCoordinates(), vector.getCoordinates().length);
     }
 
-    public Vector(double[] coordinates) {    // 1.c
+    public Vector(String vectorName, double[] coordinates) {    // 1.c. Vector(double[]) – заполнение вектора значениями из массива
+        this.vectorName = vectorName;
         this.coordinates = coordinates;
     }
 
-    public Vector(double[] coordinates, int dimension) {    // 1.d
+    // 1.Vector(n, double[]) – заполнение вектора значениями из массива. Если длина массива меньше n, то считать что в остальных компонентах 0
+    public Vector(String vectorName, double[] coordinates, int dimension) {
+        this.vectorName = vectorName;
         this.coordinates = Arrays.copyOf(coordinates, dimension);
     }
 
-    // Методы
+    // ================== Методы
     public double[] getCoordinates() {
         return coordinates;
     }
@@ -37,9 +45,25 @@ public class Vector {
         this.coordinates = coordinates;
     }
 
+    // Реализовать метод toString(), чтобы выдавал информацию о векторе в формате { значения компонент через запятую }
     @Override
     public String toString() {      // 3.
-        return "Vector" + Arrays.toString(coordinates);
+        //return vectorName + Arrays.toString(coordinates);
+        String response = "{";
+        String actualDelimiter;
+
+        for (int i = 0; i < coordinates.length; i++
+        ) {
+            if (i < coordinates.length - 1) {
+                actualDelimiter = ", ";
+            } else {
+                actualDelimiter = "}";
+            }
+
+            response = response + String.valueOf(coordinates[i]) + actualDelimiter;
+        }
+
+        return vectorName + response;
     }
 
 
