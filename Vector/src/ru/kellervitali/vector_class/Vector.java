bbfedd60546e76a46f0b47d499bf434a56fb44da
v.keller <v.keller@inbox.ru>
2020-10-11 20:09:04 +0700

    Почти все, кроме 5.c

--- Vector/src/ru/kellervitali/vector_class/Vector.java
+++ Vector/src/ru/kellervitali/vector_class/Vector.java
@@ -5,36 +5,31 @@ import java.util.Arrays;
 public class Vector {
     // ================= ПОЛЯ
 
-    private final String vectorName;
     private double[] coordinates;
 
     // ================= КОНСТРУКТОРЫ
 
     // 1.a. Vector(n) – размерность n, все компоненты равны
-    public Vector(String vectorName, int dimension) {
+    public Vector(int dimension) {
         if (dimension <= 0) {
             throw new IllegalArgumentException("Ошибка! dimension должно быть \">0\"!");
         }
 
-        this.vectorName = vectorName;
         this.coordinates = new double[dimension];
     }
 
     // 1.b. Vector(Vector) – конструктор копирования
-    public Vector(String vectorName, Vector vector) {
-        this.vectorName = vectorName;
+    public Vector(Vector vector) {
         this.coordinates = Arrays.copyOf(vector.getCoordinates(), vector.getCoordinates().length);
     }
 
     // 1.c. Vector(double[]) – заполнение вектора значениями из массива
-    public Vector(String vectorName, double[] coordinates) {
-        this.vectorName = vectorName;
+    public Vector(double[] coordinates) {
         this.coordinates = coordinates;
     }
 
     // 1.Vector(n, double[]) – заполнение вектора значениями из массива. Если длина массива меньше n, то считать что в остальных компонентах 0
-    public Vector(String vectorName, double[] coordinates, int dimension) {
-        this.vectorName = vectorName;
+    public Vector(double[] coordinates, int dimension) {
         this.coordinates = Arrays.copyOf(coordinates, dimension);
     }
 
@@ -62,19 +57,53 @@ public class Vector {
             response = response + String.valueOf(coordinates[i]) + actualDelimiter;
         }
 
-        return vectorName + response;
+        return response;
     }
 
-    //4.a. Прибавление к вектору другого вектора
+    // 4.a. Прибавление к вектору другого вектора
     public double[] addVector(Vector vector) {
         int size = Math.max(this.getSize(), vector.getSize());
-        double[] addVectorResult= new double[size];
+        double[] addVectorResult = new double[size];
 
         for (int i = 0; i < size; i++) {
-            addVectorResult[i] = this.getCoordinate(i) + vector.getCoordinate(i);
+            addVectorResult[i] = this.getCoordinateByIndex(i) + vector.getCoordinateByIndex(i);
         }
 
         return addVectorResult;
+    }
+
+    // 4.b. Вычитание из вектора другого вектора
+    public double[] subtractVector(Vector vector) {
+        int size = Math.max(this.getSize(), vector.getSize());
+        double[] addVectorResult = new double[size];
+
+        for (int i = 0; i < size; i++) {
+            addVectorResult[i] = this.getCoordinateByIndex(i) - vector.getCoordinateByIndex(i);
+        }
+
+        return addVectorResult;
+    }
+
+    //  4.c. Умножение вектора на скаляр
+    public double[] scalarVector(Vector vector, double scalarValue) {
+        double[] addVectorResult = new double[vector.getSize()];
+
+        for (int i = 0; i < vector.getSize(); i++) {
+            addVectorResult[i] = this.getCoordinateByIndex(i) * scalarValue;
+        }
+
+        return addVectorResult;
+    }
+
+    //  4.d. Разворот вектора (умножение всех компонент на -1)
+    public double[] reverceVector(Vector vector) {
+        double[] reverceVectorResult = new double[vector.getSize()];
+
+        for (int i = 0; i < vector.getSize(); i++) {
+            reverceVectorResult[i] = this.getCoordinateByIndex(i) * -1;
+        }
+
+        return reverceVectorResult;
     }
 
     // 4.e Получение длины вектора
@@ -91,7 +120,7 @@ public class Vector {
     }
 
     // 4.f.1 Получение компоненты вектора по индексу
-    public double getCoordinate(int coordinateIndex) {
+    public double getCoordinateByIndex(int coordinateIndex) {
         if (coordinateIndex >= this.coordinates.length) {
             return 0;
         }
@@ -100,9 +129,9 @@ public class Vector {
     }
 
     // 4.f.2 Установка компоненты вектора по индексу
-    public void setCoordinate(int coordinateIndex, double coordinateValue) {
+    public void setCoordinateByIndex(int coordinateIndex, double coordinateValue) {
         if (coordinateIndex >= this.coordinates.length) {
-            // ---------------;
+            this.coordinates = Arrays.copyOf(this.coordinates, coordinateIndex);
         }
 
         this.coordinates[coordinateIndex] = coordinateValue;
@@ -114,5 +143,43 @@ public class Vector {
 
     public double[] getCoordinates() {
         return coordinates;
+    }
+
+    // 4.g Переопределить метод equals, чтобы был true ó векторы имеют одинаковую размерность и соответствующие компоненты равны.
+    // Соответственно, переопределить hashCode
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Vector vector = (Vector) o;
+        return Arrays.equals(coordinates, vector.coordinates);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 37;
+        return prime + Arrays.hashCode(coordinates);
+    }
+
+    public static Vector addVector(Vector vector1, Vector vector2) {
+        Vector newResultVector = new Vector(vector1);
+
+        newResultVector.addVector(vector2);
+        return newResultVector;
+    }
+
+    public static Vector subtractVector(Vector vector1, Vector vector2) {
+        Vector newResultVector = new Vector(vector1);
+
+        newResultVector.subtractVector(vector2);
+        return newResultVector;
     }
 }
--- Vector/src/ru/kellervitali/vector_main/Main.java
+++ Vector/src/ru/kellervitali/vector_main/Main.java
@@ -6,29 +6,39 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Vector vector1 = new Vector("vector1", new double[]{-1.1, 4, 2});
-        Vector vector2 = new Vector("vector2", new double[]{4, 2, 1, 5});
-        Vector vector3 = new Vector("vector3", vector2);
-        Vector vector4 = new Vector("vector4", new double[]{3, 5, 7}, 5);
+        Vector vector1 = new Vector(new double[]{-1.1, 4, 2});
+        Vector vector2 = new Vector(new double[]{4, 2, 1, 5});
+        Vector vector3 = new Vector(vector2);
+        Vector vector4 = new Vector(new double[]{3, 5, 7}, 5);
 
-        System.out.println(vector1 + ", размерность = " + vector1.getSize() + ", длина = " + vector1.getLength());
-        System.out.println(vector2 + ", размерность = " + vector2.getSize() + ", длина = " + vector2.getLength());
-        System.out.println(vector3 + ", размерность = " + vector3.getSize() + ", длина = " + vector3.getLength());
-        System.out.println(vector4 + ", размерность = " + vector4.getSize() + ", длина = " + vector4.getLength());
+        System.out.println(vector1 + ", размерность = " + vector1.getSize() + ", длина = " + vector1.getLength() + ", hash = " + vector1.hashCode());
+        System.out.println(vector2 + ", размерность = " + vector2.getSize() + ", длина = " + vector2.getLength() + ", hash = " + vector2.hashCode());
+        System.out.println(vector3 + ", размерность = " + vector3.getSize() + ", длина = " + vector3.getLength() + ", hash = " + vector3.hashCode());
+        System.out.println(vector4 + ", размерность = " + vector4.getSize() + ", длина = " + vector4.getLength() + ", hash = " + vector4.hashCode());
         //System.out.println(vector5);
 
         try {
-            Vector vector5 = new Vector("vector5", 2);
+            Vector vector5 = new Vector(2);
             vector5.setCoordinates(new double[]{1, 2});
-            vector5.setCoordinate(1, 5.5);
+            vector5.setCoordinateByIndex(1, 5.5);
             System.out.println(vector5);
 
-            Vector vector666 = new Vector("vector666", -1);
+            Vector vector666 = new Vector(-1);
             System.out.println(vector666);
         } catch (Throwable e) {
             System.out.println(e.getMessage());
         }
 
-        System.out.println("Координаты сложения вектора 1 и 2: " + Arrays.toString(vector1.addVector(vector2)));
+        System.out.println();
+        System.out.println("4.a. Координаты сложения вектора 1 и 2: " + Arrays.toString(vector1.addVector(vector2)));
+        System.out.println("4.b. Координаты вычитания из вектора 1 вектора 2: " + Arrays.toString(vector1.subtractVector(vector2)));
+        System.out.println("4.c. Координаты умножения вектора 1 на скалар (3): " + Arrays.toString(vector1.scalarVector(vector1, 3)));
+        System.out.println("4.d. Координаты разворота вектора 1: " + Arrays.toString(vector1.reverceVector(vector1)));
+
+        System.out.println();
+        System.out.println("(1 == 2) " + vector1.equals(vector2));
+        System.out.println("(2 == 3) " + vector2.equals(vector3));
+        System.out.println("(3 == 4) " + vector3.equals(vector4));
+        System.out.println("(4 == 1) " + vector4.equals(vector1));
     }
 }
