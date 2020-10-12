e74085673f6673dea61a2bc1fe4281c926ca8152
v.keller <v.keller@inbox.ru>
2020-10-13 02:16:33 +0700

    Разложил поля на индивидуальные строки, вместо одной строки.

--- Shapes/src/ru/kellervitali/shape_classes/Triangle.java
+++ Shapes/src/ru/kellervitali/shape_classes/Triangle.java
@@ -1,7 +1,12 @@
 package ru.kellervitali.shape_classes;
 
 public class Triangle implements Shape {
-    private double x1, y1, x2, y2, x3, y3;
+    private double x1;
+    private double y1;
+    private double x2;
+    private double y2;
+    private double x3;
+    private double y3;
 
     public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
         this.x1 = x1;
