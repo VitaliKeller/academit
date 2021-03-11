1d72b7f3ca6c7d4eae42936654908c66f2b40442
kellervitali <v.keller@inbox.ru>
2021-03-11 08:48:30 +0700

    Играюшки с компаратором

---
+++ _Compare_testing/src/ru/kellervitali/Car.java
@@ -0,0 +1,20 @@
+package ru.kellervitali;
+
+public class Car implements Comparable<Car>{
+    private int manufactureYear;
+    private String model;
+    private int maxSpeed;
+
+    public Car(int manufactureYear, String model, int maxSpeed) {
+        this.manufactureYear = manufactureYear;
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return this.manufactureYear - o.manufactureYear;
+    }
+
+
+}
---
+++ _Compare_testing/src/ru/kellervitali/Compare_main.java
@@ -0,0 +1,39 @@
+package ru.kellervitali;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Compare_main {
+    public static void main(String[] args) {
+
+        String name1 = "Маша";
+        String name2 = "Саша";
+        String name3 = "Даша";
+
+        List<String> names = new ArrayList<>();
+        names.add(name1);
+        names.add(name2);
+        names.add(name3);
+
+        Collections.sort(names);
+        System.out.println(names);
+
+        // https://javarush.ru/groups/posts/2262-comparator-v-java
+
+        List<Car> cars = new ArrayList<>();
+
+        Car ferrari = new Car(1990, "Ferrari 360 Spider", 310);
+        Car lambo = new Car(20012, "Lamborghini Gallardo", 290);
+        Car bugatti = new Car(2010, "Bugatti Veyron", 350);
+
+        cars.add(ferrari);
+        cars.add(bugatti);
+        cars.add(lambo);
+
+        //ошибка компилятора!
+        Collections.sort(cars);
+
+    }
+}
