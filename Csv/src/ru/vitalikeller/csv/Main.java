1a2db85274deac46d5068b1ffd63a6f33856d931
v.keller <v.keller@inbox.ru>
2021-06-07 14:50:06 +0700

    CSV - правка

--- Csv/src/ru/vitalikeller/csv/Main.java
+++ Csv/src/ru/vitalikeller/csv/Main.java
@@ -2,6 +2,8 @@ package ru.vitalikeller.csv;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Main {
@@ -9,25 +11,30 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите название файла CSV:");
-        String fileName = scanner.nextLine();
+        System.out.print("Введите название файла CSV: ");
+        //String fileName = scanner.nextLine();
+        String fileName = "text.csv";
+        System.out.println(fileName);
 
-        System.out.println("Загрузка CSV...");
+        System.out.println("... Загрузка файла.");
         StringBuilder csvContent = readCsvFile(fileName);
 
-        System.out.println("Ковертация в HTML:" + fileName);
+        System.out.println("... Конвертация в HTML: " + fileName);
         StringBuilder htmlContent = convertCsvToHtml(csvContent);
 
+        System.out.println("... вывод в файл ");
+        String outFileName = "index.html";
+        saveHtmlContentToFile(outFileName, htmlContent);
     }
 
     private static StringBuilder readCsvFile(String fileName) {
         StringBuilder csvContent = new StringBuilder();
-        try (Scanner scannerFile = new Scanner(new FileInputStream(fileName), "utf-8")) {
+        try (Scanner scannerFile = new Scanner(new FileInputStream(fileName), StandardCharsets.UTF_8)) {
             while (scannerFile.hasNextLine()) {
                 csvContent.append(scannerFile.nextLine()).append("\n");
             }
         } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Ошибка: " + e.getMessage());
         }
 
         return csvContent;
@@ -37,20 +44,24 @@ public class Main {
         StringBuilder contentHTML = new StringBuilder();
 
         contentHTML.append("<html>\n");
-        contentHTML.append("    <head> HTML </head>\n");
+        contentHTML.append("    <head> CSV to HTML </head>\n");
         contentHTML.append("    <body>\n");
-        //contentHTML.append("        <table cols = \"5\" alignt = \"center\" border = \"1%\">\n");
-        boolean tdIsOpen = false;
+        contentHTML.append("        <table>\n");
+
+        // boolean trIsOpen = false;
+        boolean tdIsOpen = true;
 
         for (String row : contentCSV.toString().split("\\n")) {
+            // открыть строку, если ячейка не открыта
             if (!tdIsOpen) {
-                contentHTML.append("        <tr align = \"center\">\n");
+                contentHTML.append("          <tr>\n");
             }
 
-            //tdIsOpen = makeCells(contentHTML, tdIsOpen, row);
+            // тут обработка строки
 
+            // закрыть строку, если ячейка не открыта
             if (!tdIsOpen) {
-                contentHTML.append("        </tr>\n");
+                contentHTML.append("          </tr>\n");
             }
         }
 
@@ -61,4 +72,12 @@ public class Main {
         return contentHTML;
     }
 
+
+    private static void saveHtmlContentToFile(String outFileName, StringBuilder htmlContent) {
+        try (PrintWriter writer = new PrintWriter(outFileName)) {
+            writer.print(htmlContent);
+        } catch (FileNotFoundException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
 }
