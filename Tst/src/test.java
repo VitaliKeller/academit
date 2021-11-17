cf089044f511064ac3023687ea3e002daba3ea6d
v.keller <v.keller@inbox.ru>
2021-11-17 19:13:30 +0700

    ArrayList - еще в tst поковырял

--- Tst/src/test.java
+++ Tst/src/test.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class test {
     public static void main(String[] args) {
         Test1 a1 = new Test1();
@@ -9,7 +11,12 @@ public class test {
 
         public void start() {
             System.out.println(modCount);
-        }
 
+            int[] ar1 = {1, 2, 3};
+            int[] ar2 = new int[1];
+                    ar2 = Arrays.copyOf(ar1, ar1.length);
+
+            System.out.println(Arrays.toString(ar2));
+        }
     }
 }
